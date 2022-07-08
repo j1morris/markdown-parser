ea8d4d33c7e5f4ca647f1fc14e788edf7213d729
j1morris <j1morris@ucsd.edu>
2022-07-08 11:08:09 -0700

    added tests and fixed their bugs

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -1,10 +1,31 @@
 import static org.junit.Assert.*;
 import org.junit.*;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.List;
+
 public class MarkdownParseTest {
 
     @Test
     public void addition() {
         assertEquals(2, 1 + 1);
     }
+
+    @Test
+    public void testFile1() throws IOException {
+        String contents= Files.readString(Path.of("./test-file.md"));
+        List<String> expect = List.of("https://something.com", "some-thing.html");
+        assertEquals(expect, MarkdownParse.getLinks(contents));
+    }
+
+    @Test
+    public void testFile2() throws IOException {
+        String contents= Files.readString(Path.of("./test-file2.md"));
+        List<String> expect = List.of("https://google.com", "some-thing.html");
+        assertEquals(expect, MarkdownParse.getLinks(contents));
+    }
+
 
 }
